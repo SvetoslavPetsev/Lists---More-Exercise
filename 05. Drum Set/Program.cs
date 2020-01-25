32dceb4a1c5a49ae0ccf3926f9cbe3e8c0c466c5
Svetoslav Petsev <57420437+SvetoslavPetsev@users.noreply.github.com>
2020-01-26 00:46:48 +0200

    Update Program.cs

--- 05. Drum Set/Program.cs
+++ 05. Drum Set/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections.Generic;
 namespace _05._Drum_Set
@@ -7,7 +7,63 @@ namespace _05._Drum_Set
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            double startMoney = double.Parse(Console.ReadLine());
+
+            List<int> drumSetStart = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> drumSetActual = new List<int>();
+            for (int i = 0; i < drumSetStart.Count; i++)
+            {
+                drumSetActual.Insert(i, drumSetStart[i]);
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Hit it again, Gabsy!")
+                {
+                    break;
+                }
+
+                int hitPower = int.Parse(input);
+
+                for (int i = 0; i < drumSetActual.Count; i++)
+                {
+                    if (drumSetActual[i] - hitPower < 0)
+                    {
+                        drumSetActual[i] = 0;
+                        continue;
+                    }
+                    else
+                    {
+                        drumSetActual[i] -= hitPower;
+                    }
+                }
+                if (drumSetActual.Contains(0))
+                {
+                    for (int i = 0; i < drumSetActual.Count; i++)
+                    {
+                        if (drumSetActual[i] == 0)
+                        {
+                            if (startMoney - drumSetStart[i] * 3.0 >= 0)
+                            {
+                                drumSetActual[i] = drumSetStart[i];
+                                startMoney -= drumSetStart[i] * 3.0;
+                            }
+                            else
+                            {
+                                drumSetActual.RemoveAt(i);
+                                drumSetStart.RemoveAt(i);
+                            }
+                        }
+                    }
+                } 
+            }
+            Console.WriteLine(string.Join(" ", drumSetActual));
+            Console.WriteLine($"Gabsy has {startMoney:F2}lv.");
         }
     }
 }
